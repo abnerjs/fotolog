f2fd76eacb9b47129e8266332ac2328b4ce2eb42
abnerjs <abner.js05@gmail.com>
2021-02-15 14:59:05 -0300

    ????

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,21 +1,24 @@
-import { Request, Response } from "express";
-import { getRepository, IsNull } from "typeorm";
-import User from "../models/User";
-import userView from "../views/user-view";
-import * as jwt from "jsonwebtoken";
+import { Request, Response } from 'express';
+import { getRepository, IsNull } from 'typeorm';
+import User from '../models/User';
+import userView from '../views/user-view';
+import * as jwt from 'jsonwebtoken';
 process.env.ACCESS_TOKEN_SECRET as jwt.Secret;
 
 export default {
   async create(request: Request, response: Response) {
     //desestruturar o corpo da requisição (JSON)
     console.log(request.body);
- 
+
     const { nome, login, senha, tipo } = request.params;
 
     const userRepository = getRepository(User);
 
     const user = userRepository.create({
-      nome, login, senha, tipo
+      nome,
+      login,
+      senha,
+      tipo,
     });
 
     await userRepository.save(user);
@@ -43,36 +46,37 @@ export default {
       const { login, senha } = request.body;
 
       //Verificação somente para testes (substituir)
-      
+
       const userRepository = getRepository(User);
 
       const parametros = [login, senha];
-      const usuario = await userRepository.query("select * from user where login = ? and senha = ?", parametros)
+      const usuario = await userRepository.query(
+        'select * from user where login = ? and senha = ?',
+        parametros
+      );
 
       console.log(usuario[0]);
-      
-      
-      if ( usuario!=undefined) {
+
+      if (usuario != undefined) {
         const user = userView.render(usuario[0]);
-      console.log(user);
-        
-        const token = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET as jwt.Secret, {
-          expiresIn: 300, // expires in 5min
-        });
+        console.log(user);
+
+        const token = jwt.sign(
+          { user },
+          process.env.ACCESS_TOKEN_SECRET as jwt.Secret,
+          {
+            expiresIn: 300, // expires in 5min
+          }
+        );
         return response.status(200).json({
           token: token,
-          user:user
+          user: user,
         });
       }
-
-
-
     } catch (err) {
       console.log(err);
-      return response.status(500).json({ message: "Login inválido!" });
+      return response.status(500).json({ message: 'Login inválido!' });
     }
-    return response.status(500).json({ message: "Login inválido!" });
+    return response.status(500).json({ message: 'Login inválido!' });
   },
-
-
 };
--- backend/src/views/user-view.ts
+++ backend/src/views/user-view.ts
@@ -1,19 +1,19 @@
-
-import User from "../models/User";
-import postView from "./post-view";
+import User from '../models/User';
+import postView from './post-view';
 
 export default {
   render(user: User) {
     return {
       id: user.id,
       nome: user.nome,
-     // posts: postView.renderMany(user.posts),
+      // posts: postView.renderMany(user.posts),
       login: user.login,
-      tipo:user.tipo
-    };0
+      tipo: user.tipo,
+    };
+    0;
   },
   renderMany(users: User[]) {
-      console.log(users);
+    console.log(users);
     return users.map((user) => this.render(user));
   },
 };
